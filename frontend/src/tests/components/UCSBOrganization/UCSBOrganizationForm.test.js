4ef415d3727203c450a1c5f77d27bda01d676077
Mu Chen <mu52@ucsb.edu>
2023-11-04 22:33:30 -0700

    added form

--- frontend/src/tests/components/UCSBOrganization/UCSBOrganizationForm.test.js
+++ frontend/src/tests/components/UCSBOrganization/UCSBOrganizationForm.test.js
@@ -1,7 +1,7 @@
 import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import { BrowserRouter as Router } from "react-router-dom";
 
-import UCSBOrganizationFormForm from "main/components/UCSBOrganization/UCSBOrganizationForm";
+import UCSBOrganizationForm from "main/components/UCSBOrganization/UCSBOrganizationForm";
 import { ucsbOrganizationFixtures } from "fixtures/ucsbOrganizationFixtures";
 
 import { QueryClient, QueryClientProvider } from "react-query";
@@ -13,17 +13,17 @@ jest.mock('react-router-dom', () => ({
     useNavigate: () => mockedNavigate
 }));
 
-describe("RestaurantForm tests", () => {
+describe("UCSBOrganizationForm tests", () => {
     const queryClient = new QueryClient();
 
-    const expectedHeaders = ["Name", "Description"];
-    const testId = "RestaurantForm";
+    const expectedHeaders = ["OrgTranslationShort", "OrgTranslation", "Inactive"];
+    const testId = "UCSBOrganizationForm";
 
     test("renders correctly with no initialContents", async () => {
         render(
             <QueryClientProvider client={queryClient}>
                 <Router>
-                    <RestaurantForm />
+                    <UCSBOrganizationForm />
                 </Router>
             </QueryClientProvider>
         );
@@ -41,7 +41,7 @@ describe("RestaurantForm tests", () => {
         render(
             <QueryClientProvider client={queryClient}>
                 <Router>
-                    <RestaurantForm initialContents={restaurantFixtures.oneRestaurant} />
+                    <UCSBOrganizationForm initialContents={UCSBOrganizationFixtures.oneOrganization} />
                 </Router>
             </QueryClientProvider>
         );
@@ -53,8 +53,8 @@ describe("RestaurantForm tests", () => {
             expect(header).toBeInTheDocument();
         });
 
-        expect(await screen.findByTestId(`${testId}-id`)).toBeInTheDocument();
-        expect(screen.getByText(`Id`)).toBeInTheDocument();
+        expect(await screen.findByTestId(`${testId}-orgCode`)).toBeInTheDocument();
+        expect(screen.getByText(`OrgCode`)).toBeInTheDocument();
     });
 
 
@@ -62,7 +62,7 @@ describe("RestaurantForm tests", () => {
         render(
             <QueryClientProvider client={queryClient}>
                 <Router>
-                    <RestaurantForm />
+                    <UCSBOrganizationForm />
                 </Router>
             </QueryClientProvider>
         );
@@ -78,7 +78,7 @@ describe("RestaurantForm tests", () => {
         render(
             <QueryClientProvider client={queryClient}>
                 <Router>
-                    <RestaurantForm />
+                    <UCSBOrganizationForm />
                 </Router>
             </QueryClientProvider>
         );
@@ -87,16 +87,22 @@ describe("RestaurantForm tests", () => {
         const submitButton = screen.getByText(/Create/);
         fireEvent.click(submitButton);
 
-        await screen.findByText(/Name is required/);
-        expect(screen.getByText(/Description is required/)).toBeInTheDocument();
+        await screen.findByText(/OrgTranslationShort is required/);
+        expect(screen.getByText(/OrgTranslation is required/)).toBeInTheDocument();
 
-        const nameInput = screen.getByTestId(`${testId}-name`);
-        fireEvent.change(nameInput, { target: { value: "a".repeat(31) } });
+        const orgTranslationShortInput = screen.getByTestId(`${testId}-orgTranslationShort`);
+        fireEvent.change(orgTranslationShortInput, { target: { value: "a".repeat(31) } });
         fireEvent.click(submitButton);
 
         await waitFor(() => {
             expect(screen.getByText(/Max length 30 characters/)).toBeInTheDocument();
+        
         });
+        
+        expect(inactiveCheckbox).toBeInTheDocument();
+        const inactiveCheckbox = screen.getByTestId(`${testId}-inactive`);
+        fireEvent.click(inactiveCheckbox);
+
     });
 
 });
