1376775c73b3ccb1d7d4ef3c9d01e5d0b65d929e
Adyah Rastogi <adyahrastogi@gmail.com>
2023-11-07 14:17:10 -0800

    Created fixtures and added examples

---
+++ frontend/src/fixtures/recommendationRequestFixtures.js
@@ -0,0 +1,49 @@
+const recommendationRequestFixtures = {
+    oneRecommendation:
+    [
+      {
+       "id": 1,
+        "requesterEmail": "cgaucho@ucsb.edu",
+        "professorEmail": "phtcon@ucsb.edu",
+        "explanation": "BS/MS program",
+        "dateRequested": "2022-04-20T00:00:00Z",
+        "dateNeeded": "2022-05-01T00:00:00Z",
+        "done": false
+      }
+    ],
+
+    threeRestaurants:
+    [
+        {
+            "id": 1,
+            "requesterEmail": "cgaucho@ucsb.edu",
+            "professorEmail": "phtcon@ucsb.edu",
+            "explanation": "BS/MS program",
+            "dateRequested": "2022-04-20T00:00:00Z",
+            "dateNeeded": "2022-05-01T00:00:00Z",
+            "done": false
+        },
+
+        {
+            "id": 2,
+            "requesterEmail": "ldelplaya@ucsb.edu",
+            "professorEmail": "richert@ucsb.edu",
+            "explanation": "PhD CS Stanford",
+            "dateRequested": "2022-05-20T00:00:00Z",
+            "dateNeeded": "2022-11-15T00:00:00Z",
+            "done": false
+        },
+
+        {
+            "id": 3,
+            "requesterEmail": "ldelplaya@ucsb.edu",
+            "professorEmail": "phtcon@ucsb.edu",
+            "explanation": "PhD CS Stanford",
+            "dateRequested": "2022-05-20T00:00:00Z",
+            "dateNeeded": "2022-11-15T00:00:00Z",
+            "done": false
+        }        
+    ]
+};
+
+export { restaurantFixtures };
